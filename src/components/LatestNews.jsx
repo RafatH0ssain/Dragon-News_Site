e51567595074cb1eef125dc2825b4358ee3f4ff9
RafatH0ssain <rafat.click.hossain@gmail.com>
2025-01-01 20:37:40 -0400

    Added bg color, fixing css

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -5,10 +5,10 @@ const LatestNews = () => {
     return (
         <div className="flex gap-2 items-center bg-base-300 p-2">
             <p className="bg-red-800 text-base-100 px-3 py-1">Latest</p>
-            <Marquee pauseOnHover={true}>
-                <Link to="/news">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias at dolores error voluptatibus, dolorum inventore nihil nobis itaque eos sequi!
-                </Link>
+            <Marquee pauseOnHover={true} className="bg-base-300">
+                <Link to="/news" className="bg-base-300">
+                Breaking News: Mysterious Lights Illuminate Night Sky Over Downtown
+                Residents of downtown were left baffled last night as strange, pulsating lights appeared in the sky. Eyewitnesses report colors shifting from green to blue, with a low hum echoing across the city. Authorities are investigating, though no official explanation has been given. Stay tuned for updates.</Link>
             </Marquee>
         </div>
     );
